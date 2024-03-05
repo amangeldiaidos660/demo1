33eb6ccf50959e621ac3d4b853476da1127cf8fd
Aidos Amangeldi <122286376+amangeldiaidos660@users.noreply.github.com>
2024-03-05 13:57:35 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+
+$maxRequests = 2;
+$interval = 60;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $currentTime = time();
+
+    if (!isset($_SESSION['last_request_time'][$ip]) || ($currentTime - $_SESSION['last_request_time'][$ip]) >= $interval) {
+        $_SESSION['request_count'][$ip] = 1; 
+    } else {
+        $_SESSION['request_count'][$ip] = isset($_SESSION['request_count'][$ip]) ? $_SESSION['request_count'][$ip] + 1 : 1;
+    }
+
+    if ($_SESSION['request_count'][$ip] > $maxRequests) {
+        echo 'Вы превысили лимит. Попробуйте через минуту!';
+    } else {
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+        $tel = filter_input(INPUT_POST, 'tel', FILTER_SANITIZE_STRING);
+        $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+        $data = array(
+            'p1' => $name,
+            'p2' => $tel,
+            'p3' => $message
+        );
+
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents('https://script.google.com/macros/s/AKfycbxP_3gbSU8qVQrwQbRDRhEj62jffpUiC3OHTKBj2fQ_Tq7mmit0bO1pWtCjYfWywz8p/exec', false, $context);
+
+        if ($result === FALSE) {
+            echo 'false';
+        } else {
+            $token = "5910801981:AAF1awL84ElfovtqYsa96s7dnIRcL-x-Kg0";  
+            $chat_id = "-1002135649662";  
+
+            $telegram_message = "Новая заявка!\nИмя: $name\nТелефон: $tel\nПодробнее: 'https://docs.google.com/spreadsheets/d/12xCqqmdejSes3XxHEI9uc4A11Di4RnJrTbHBK4-r5C4/edit?usp=sharing'\n";
+            $telegram_url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($telegram_message);
+
+            $telegram_result = file_get_contents($telegram_url);
+
+            if ($telegram_result === FALSE) {
+                echo 'false';
+            } else {
+                echo 'true';
+            }
+        }
+
+        $_SESSION['last_request_time'][$ip] = $currentTime;
+    }
+}
+
+?>
---
+++ src/langs.js
@@ -0,0 +1,135 @@
+const lngArr = {
+    "nav-item-1": {
+        "kz": "Басты бет",
+        "ru": "Главная",
+        "en": "Main",
+    },
+    "nav-item-2": {
+        "kz": "Қызметтер",
+        "ru": "Услуги",
+        "en": "Services",
+    },
+    "nav-item-3": {
+        "kz": "Артықшылығымыз",
+        "ru": "Преимущества",
+        "en": "Excellence",
+    },
+    "nav-item-3-1": {
+        "kz": "Артықшылығымыз",
+        "ru": "Преимущества",
+        "en": "Excellence",
+    },
+    "nav-item-4": {
+        "kz": "Байланыс",
+        "ru": "Связаться",
+        "en": "Contact",
+    },
+    "nav-item-4-1": {
+        "kz": "Байланыс",
+        "ru": "Связаться",
+        "en": "Contact",
+    },
+    "main-title":{
+        "kz": "Өзіне-өзі қызмет көрсету машиналарына арналған Kaspi QR",
+        "ru": "Kaspi QR для аппаратов самообслуживания",
+        "en": "Kaspi QR for self-service machines",
+    },
+    "main-title-2":{
+        "kz": "Өзіне-өзі қызмет көрсету машиналарына арналған Kaspi QR",
+        "ru": "Kaspi QR для аппаратов самообслуживания",
+        "en": "Kaspi QR for self-service machines",
+    },
+    "service-1": {
+        "kz": "Өзіне-өзі қызмет көрсететін<br> автокөлік жуу орындары",
+        "ru": "Автомойки<br>самообслуживания",
+        "en": "Self-service <br>car washes",
+    },
+    "service-2": {
+        "kz": "Levending(PRO LV307) kофе машиналары",
+        "ru": "Кофейные аппараты Levending(PRO LV307)",
+        "en": "Coffee machines Levending(PRO LV307)",
+    },
+    "price": {
+        "kz": "Бағасы",
+        "ru": "Стоимость",
+        "en": "Price",
+    },    
+    "price-2": {
+        "kz": "Бағасы",
+        "ru": "Стоимость",
+        "en": "Price",
+    },
+    "feature-1": {
+        "kz": "Қашықтан төлемді біріктіруге арналған жеке IT шешімі.",
+        "ru": "Собственноe IT решение для интеграции удаленной оплаты.",
+        "en": "Own IT solution for integrating remote payment.",
+    },
+    "feature-2": {
+        "kz": "Сатылым статистикасын қашықтан бақылау және жинау мүмкіндігі.",
+        "ru": "Возможность удаленного мониторинга и сбора статистики продаж с автоматов.",
+        "en": "Possibility of remote monitoring and collection of sales statistics from machines.",
+    },
+    "feature-3": {
+        "kz": "Табысыңызды арттыру, Kaspi QR Қазақстандағы ең танымал және танымал төлем әдісі болып табылады.",
+        "ru": "Увеличение вашей прибыли, Kaspi QR - самый востребованный и популярный способ оплаты в Казахстане.",
+        "en": "Increasing your profits, Kaspi QR is the most popular and popular payment method in Kazakhstan.",
+    },
+    "feature-4": {
+        "kz": "Техникалық қолдау, техникалық мамандардан жылдам жауап беру.",
+        "ru": "Техническое сопровождение,быстрое реагирование технических специалистов.",
+        "en": "Technical support, quick response from technical specialists.",
+    },
+    "contact-title": {
+        "kz": "Бізбен кез келген уақытта хабарласыңыз",
+        "ru": "Свяжитесь с нами в любое время",
+        "en": "Contact us anytime",
+    },
+    "place": {
+        "kz": "Гагарина көш. 93А",
+        "ru": "ул. Гагарина, 93A",
+        "en": "st. Gagarina, 93A",
+    },
+    "load": {
+        "kz": "Жүктелуде",
+        "ru": "Загрузка",
+        "en": "Loading",
+    },
+    "msg-1": {
+        "kz": "Сіздің өтінішіңізді алдық! Менеджер сізге жақын арада жауап береді.",
+        "ru": "Мы получили вашу заявку! В скором времени менеджер ответит вам.",
+        "en": "We have received your application! The manager will answer you shortly.",
+    },
+    "send": {
+        "kz": "Жіберу",
+        "ru": "Отправить",
+        "en": "Send",
+    },
+    "name2": {
+        kz: 'Атыңыз',
+        ru: 'Имя',
+        en: 'Name'
+      },
+    "tel2": {
+        kz: 'Телефон',
+        ru: 'Телефон',
+        en: 'Phone'
+      },
+    "commend": {
+        kz: 'Қандай қызметті қосқыңыз келеді?',
+        ru: 'Какую услугу вы бы хотели подключить?',
+        en: 'Which service would you like to connect?'
+      },
+    "error":{
+        kz: 'Қате"',
+        ru: 'Ошибка:',
+        en: 'Error:'
+
+    }
+    
+
+
+
+}
+
+
+  
